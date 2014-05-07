46584637604d868597b508166a4b653e79eebd3d
System Administrator <root@Jimmy-XP-PCs-Mac-mini.local>
2014-05-07 18:04:20 +0800

    add missed files

---
+++ hn/utils/macstats.py
@@ -0,0 +1,55 @@
+from scrapy.statscol import StatsCollector
+from datetime import datetime
+import redis
+from .monitors import datadog
+import os
+import pprint
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+STATS_KEEP = 50
+
+class MacStatsCollector(StatsCollector):
+    def __init__(self, crawler):
+        super(MacStatsCollector, self).__init__(crawler)
+        self.spider_stats = {}
+
+    def _persist_stats(self, stats, spider):
+        self.spider_stats[spider.name] = stats
+        #statsd metric
+        cost = (stats['finish_time'] - stats['start_time']).total_seconds()
+        norm_cost = cost//60
+        tag = str(spider)
+        datadog.gauge('crawler.crawl.cost', cost, tags=[tag])
+        datadog.gauge('crawler.crawl.cost.norm_in_minute', norm_cost, tags=[tag])
+        datadog.gauge('es.index.success', stats['es/index/success'], tags=[tag])
+        datadog.gauge('es.index.failed', stats['es/index/failed'], tags=[tag])
+        datadog.gauge('crawler.scraped.count', stats.get('item_scraped_count', 0), tags=[tag])
+        excep_count = 0
+        for k in stats:
+            if k.startswith('spider_exceptions'):
+                excep_count += stats[k]
+        datadog.gauge('crawler.spider.exceptions', excep_count, tags=[tag])
+
+        #save running stats to local file
+        date = datetime.utcnow().date()
+        host = spider.settings.get('REDIS_HOST', REDIS_HOST)
+        port = spider.settings.get('REDIS_PORT', REDIS_PORT)
+        num = spider.settings.get('STATS_KEEP', STATS_KEEP)
+
+        server = redis.Redis(host, port)
+        key = "%s:%s" % (spider, "stats")
+        server.lpush(key, stats)
+        #keep the latest num stats in redis for each crawler
+        server.ltrim(key, 0, num-1)
+
+        rootdir = spider.settings.get('STATS_LOG_PATH', '/var/log/scrapyd/stats/')
+        filename = "Stats_%s_%s.log" % (spider._type, date)
+        filename = os.path.join(rootdir, filename)
+        
+        with open(filename, 'a+') as fs:
+            stats = pprint.pformat(stats)
+            fs.write(stats + '\n\n')
+
+    def close_spider(self, spider, reason):
+        super(MacStatsCollector, self).close_spider(spider, reason)
---
+++ hn/utils/monitors.py
@@ -0,0 +1,107 @@
+import time
+import logging
+import contextlib
+import functools
+from scrapy import log
+
+import redis
+
+__all__ = ('agent', 'datadog', 'counter')
+
+
+class Agent(object):
+    def time(key, t):
+        pass
+
+    def span(key, d):
+        pass
+
+
+class LogAgent(object):
+    def __init__(self):
+        self.logger = logging.getLogger("LogAgent")
+
+    def time(self, key, t):
+        self.logger.info("%s: %s", key, t)
+
+    def span(self, key, d):
+        self.logger.info("%s: %s", key, d)
+
+
+class RedisAgent(object):
+    def __init__(self):
+        self.redis = redis.Redis(db=5)
+
+    def time(self, key, t):
+        self.redis.set(key, t)
+
+    def span(self, key, d):
+        for k, v in d.items():
+            self.redis.hset(key, k, v)
+
+    def report(self, key):
+        d = self.redis.hgetall(key)
+        for k, v in d.items():
+            d[k] = float(v)
+        return d
+
+agent = RedisAgent()
+
+
+def noop(*args, **kwargs):
+    pass
+
+
+class Datadog(object):
+    def __init__(self):
+        pass
+
+    def __getattr__(self, attr):
+        return noop
+
+    def _timed(self, func):
+        @functools.wraps(func)
+        def wrap(*args, **kwargs):
+            s = time.time()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                c = time.time() - s
+                log.msg("Timing: %s, %s" % (func, c))
+        return wrap
+
+    def timed(self, *args, **kwargs):
+        return self._timed
+
+    def increment(self, key, value=1, tags=None):
+        if tags:
+            log.msg("Increment: %s, %s, %s" % (key, value, tags))
+        else:
+            log.msg("Increment: %s, %s" % (key, value))           
+
+    def gauge(self, key, value, tags=None):
+        if tags:
+            log.msg("Gauge: %s, %s, %s" % (key, value, tags))
+        else:
+            log.msg("Gauge: %s, %s" % (key, value))
+
+try:
+    from statsd import statsd as datadog
+except:
+    datadog = Datadog()
+
+
+def counter(key):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            datadog.increment(key)
+            return func(*args, **kwargs)
+        return wrapped
+    return decorator
+
+
+@contextlib.contextmanager
+def counter_context(key):
+    yield
+    datadog.increment(key)
